a21900d743af5869eb64ee80a342f599fbbc7eaf
Sydney Michael <m13s19madden@gmail.com>
2020-03-10 17:17:17 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function destructivelyAddElementToBeginningOfArray(arr, elem) {
+  arr.unshift(elem)
+  return arr
+}
+
+function addElementToBeginningOfArray(arr, elem) {
+  var arr2 = arr
+  arr2.unshift(elem)
+  return arr2
+}
+
+function destructivelyAddElementToEndOfArray(arr, elem) {
+  arr.push(elem)
+  return arr
+}
+
+function addElementToEndOfArray(arr, elem) {
+  var arr2 = arr
+  arr2.push(elem)
+  return arr2
+}
+
+function accessElementInArray(arr, n) {
+  return arr[n]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  return arr.shift()
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  arr.slice(1)
+  return arr
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  return arr.pop()
+}
+
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(0, arr.length -1)
+}
